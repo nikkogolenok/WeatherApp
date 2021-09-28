96fae7e2a62f789bcfd7bf1e92251b1dbed04f7c
nikita_kogolenok <nikiton17ns@gmail.com>
2021-09-28 14:47:45 +0300

    Start coding

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Variables
+    
+    // MARK: - Outlet
+    
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    // MARK: - Methods
+    
+    // MARK: - Actions
 }
 
